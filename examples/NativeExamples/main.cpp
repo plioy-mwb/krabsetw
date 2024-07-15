b3a932226a1ebb7bc1bba8cf42f35647445cb35b
Paolo Lioy <plioy@malwarebytes.com>
2024-07-15 10:08:38 +0200

    consolidation

--- examples/NativeExamples/main.cpp
+++ examples/NativeExamples/main.cpp
@@ -1,19 +1,24 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include "examples.h"
 
-int main(void)
-{
-    // Comment in/out the particular example you'd like to run.
+#include "examples.h"
+#include <iostream>
+
+using namespace std;
+
+int                                     main                                (void) {
+    cout << "here we go .." << endl;
     //kernel_and_user_trace_001::start();
     //kernel_trace_001::start();
     //kernel_trace_002::start();
     //kernel_trace_003_rundown::start();
     //multiple_providers_001::start();
     //testing_001::start();
-    user_trace_001::start();
-    //user_trace_002::start();
+    //user_trace_001::start();
+    
+    user_trace_002::start(); // Powershell
+
     //user_trace_003_no_predicates::start();
     //user_trace_004::start();
     //user_trace_005::start();
